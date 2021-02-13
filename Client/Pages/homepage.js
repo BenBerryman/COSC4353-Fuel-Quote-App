69faa10f88a94799779a209e358f813b718e0c0a
Ben Berryman <ben.a.berryman@gmail.com>
2021-02-12 23:45:03 -0600

    Start of a homepage

---
+++ Client/Pages/homepage.js
@@ -0,0 +1,11 @@
+function homepageStart() {
+    $("#slideshow > img:gt(0)").hide();
+    setInterval(function() {
+        $('#slideshow > img:first')
+            .fadeOut(2500)
+            .next()
+            .show()
+            .end()
+            .appendTo('#slideshow');
+    }, 25000);
+}
