c03d2068d0ed9822995ec8e27976fee61a3ddc4a
Ben Berryman <ben.a.berryman@gmail.com>
2021-02-26 17:02:07 -0600

    Clean up login/register

---
+++ Client/Pages/Login/profileSetup.js
@@ -0,0 +1,11 @@
+
+
+function createAccount() {
+    // CREATE AJAX REQUEST HERE
+}
+
+document.querySelector(".register")
+    .addEventListener('click', (event) => {
+        createAccount();
+        event.preventDefault();
+    })
