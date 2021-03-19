4fad105d49f852dea3683b98a3f104b7b8a95e19
alexrmartin <43216063+alexrmartin@users.noreply.github.com>
2021-03-19 00:32:57 -0500

    Add files via upload

--- Client/FuelQuote/quote.js
+++ Client/FuelQuote/quote.js
@@ -15,21 +15,56 @@ function stepDown(inputField) {
 function checkValid(input) {
     if (input.value < 1) {
         input.value = null;
+    }else {
+        let val= input.value;
+        document.getElementById("dueAmt").innerHTML = (val*4.50)+"";
     }
 }
 
-async function confirmPurchase(addressNum, fuelAmount, deliveryDate) {
+async function confirmPurchase(address, quantity, deliveryDate, amount) {
     let resp = await fetch("http://localhost:5000/purchaseConfirm",
         {method: "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({
-                addressNum: addressNum,
-                fuelAmount: fuelAmount,
-                deliveryDate: deliveryDate
-            })});
+                address: address,
+                quantity: quantity,
+                deliveryDate: deliveryDate,
+                amount: amount
+            })
+        });
     return resp.status;
 }
+
+async function purchaseHistory() {
+    let resp = await fetch("http://localhost:5000/")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(myJson) {
+
+            for (let i = 0; i < myJson.length; i++) {
+                let row = myJson[i];
+                $("#head").after(
+                    "<tr>" +
+                    "<td>"+row.address.addressNum+"</td>"+
+                    "<td>"+row.address.city+"</td>"+
+                    "<td>"+row.address.state+"</td>"+
+                    "<td>"+row.address.zip+"</td>"+
+                    "<td>"+row.deliveryDate+"</td>"+
+                    "<td>"+row.quantity+"</td>"+
+                    "<td>"+row.amount+"</td>"+
+                    "</tr>" );
+
+            }
+
+
+        });
+
+}
+
 $(document).ready(function() {
+    purchaseHistory();
+
     var numberIncrementField = document.querySelector(".number-incrementer")
     document.querySelector(".increment-down")
         .addEventListener('click', (event) => {
@@ -49,15 +84,30 @@ $(document).ready(function() {
 
     document.getElementsByTagName("form")[0].onsubmit = function(event) {
         event.preventDefault();
+        let quantity = document.getElementById("gallons").value;
+        let deliveryDate = document.getElementById("date").value;
+        let amount =  document.getElementById("dueAmt").innerHTML;
+
+
         let addressNum = document.getElementById("addressNum").innerHTML;
-        let fuelAmount = document.getElementById("gallons").value;
-        let deliveryDate = document.getElementById("alt-date").value;
-        confirmPurchase(addressNum, fuelAmount, deliveryDate).then(respCode => {
+        let city = document.getElementById("city").innerHTML;
+        let state = document.getElementById("state").innerHTML;
+        let zip = document.getElementById("zip").innerHTML;
+
+
+        let address = {
+            city: city,
+            state: state,
+            zip: zip,
+            addressNum: addressNum
+        };
+
+        confirmPurchase(address, quantity, deliveryDate, amount).then(respCode => {
             if (respCode == 200) {
-                document.location.href = "http://localhost:8000/FuelQuote/PurchaseConfirm.html"
+                document.location.href = "http://localhost:8000/FuelQuote/purchaseConfirm.html"
             }
         });
-    }
+    };
 
 
     var today = new Date();
