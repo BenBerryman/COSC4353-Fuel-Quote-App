dca30fb3a163a0987e90f9a6f9a5be8f9b89d2cb
alexrmartin <43216063+alexrmartin@users.noreply.github.com>
2021-03-31 18:34:10 -0500

    Add files via upload

---
+++ Server/test/dist/appTest.dev.js
@@ -0,0 +1,57 @@
+"use strict";
+
+var chai = require('chai');
+
+var assert = require('chai').assert;
+
+var app = require('../app');
+
+var chaiHttp = require('chai-http'); //Asserstion style
+
+
+chai.should();
+chai.use(chaiHttp);
+describe('Fuel Routes', function () {
+  //Tests to be performed
+  describe('Post Routes tested here.', function (done) {
+    it('it should show status of register', function (done) {
+      chai.request(app).get('/register').end(function (err, res) {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.length.should.be.eq(3);
+        done();
+      });
+    });
+    it('if you visit the wrong register route', function (done) {
+      chai.request(app).get('/registers').end(function (err, res) {
+        res.should.have.status(404);
+        done();
+      });
+    });
+    it('Login route', function (done) {
+      chai.request(app).get('/login').end(function (err, res) {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.length.should.be.eq(2);
+        done();
+      });
+    });
+    it('Logout route', function (done) {
+      chai.request(app).get('/logout').end(function (err, res) {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.should.have.property('session');
+        res.body.should.have.property('delete');
+        done();
+      });
+    });
+    it('Logout route', function (done) {
+      chai.request(app).get('/logoutd').end(function (err, res) {
+        res.should.have.status(404);
+        done();
+      });
+    });
+  }); //3.home
+  //4.Logout.
+  //5.Order.
+});
