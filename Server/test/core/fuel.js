0505699e2e8156ff95bcee529b535abca22dc9d7
alexrmartin <43216063+alexrmartin@users.noreply.github.com>
2021-04-02 16:05:44 -0500

    Add files via upload

---
+++ Server/test/core/fuel.js
@@ -0,0 +1,28 @@
+/**
+ * Simulates the fuel purchase process.
+ */
+const fuel = [
+    {
+        id: 1,
+        gallons: 20,
+        send_address: 'NY',
+        delivery_date: '11/01/2021',
+        paid:50
+    },
+    {
+      id: 2,
+      gallons: 40,
+      send_address: 'New Mexico',
+      delivery_date: '11/01/2021',
+      paid:100
+    },
+    {
+      id: 3,
+      gallons: 30,
+      send_address: 'California',
+      delivery_date: '12/03/2021',
+      paid:75
+    },
+];
+ 
+module.exports = fuel;
