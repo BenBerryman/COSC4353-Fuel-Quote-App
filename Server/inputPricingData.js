c00c3aaab844dcd55e603344fd978f39c238842a
alexrmartin <43216063+alexrmartin@users.noreply.github.com>
2021-04-26 17:25:57 -0500

    Add files via upload

---
+++ Server/inputPricingData.js
@@ -0,0 +1,35 @@
+require("dotenv").config()
+const mongoose = require("mongoose")
+const Pricing = require("./models/Pricing")
+
+// Mongoose Database Setup.
+mongoose.connect(process.env.MONGO_DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+// Database Connection.
+const db = mongoose.connection
+
+db.on("error", console.error.bind("console", "Connection To The MongoDB Failed!"))
+db.once("open", () => {
+  console.log("Successfully Connected To The MongoDB.")
+})
+
+// Set Pricing Module Into DB.
+const setPricingData = async () => {
+  try {
+    const pricingData = new Pricing()
+    await pricingData.save((err, results) => {
+      if(err) console.log(err.message)
+      db.close()
+      return results
+    })
+  }catch(err){
+    throw new TypeError(err.message)
+  }
+}
+
+setPricingData()
+
+module.exports = setPricingData
